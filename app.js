7af917729b80dd31a04812b1ddf15682601cb05b
sharad choudhary <sharadc983@gmail.com>
2026-05-01 14:16:03 +0530

    update cors

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://truebond-six.vercel.app/"
+    "https://truebond-six.vercel.app"
   ],
   credentials: true
 }));
